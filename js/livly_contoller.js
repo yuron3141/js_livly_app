6686cf51e51c2e714c9df80de16c11f652b13d17
yuron3141 <sub-2718-vrchat@yahoo.co.jp>
2022-04-16 15:42:59 +0900

    ver3.0

--- js/livly_contoller.js
+++ js/livly_contoller.js
@@ -1,119 +1,40 @@
+//キャラクター本体の取得
+const chara = document.getElementById("livly");
 
-//リヴリーの取得
-const ball = document.getElementById("livly");
-//リヴリーsvgの取得
-const pigmy = document.getElementById('main');
+//キャラの描画フレームを取得
+const standing = document.getElementById("standing");
+const walking = document.getElementById("walking");
+const gripped = document.getElementById("gripped");
+const sleeping = document.getElementById("sleeping");
 
-//移動のためのパラメータ設定
-//目的地座標
-let aimX = 0;
-let aimY = 0;
-//計算用の目的位置
-let calcX = 0;
-let calcY = 0;
-//アイドル時間
-let idletime = 1000;//(1秒)
-//現在地座標
-let nowX = 0;
-let nowY = 0;
-//角度
-let angle = 0;
-//状態bool
-let state = 1; //1:つかまれてない,2:つかまれている
-//到着bool
-let arrived = 0; //0:到着していない,1進行中,2:到着した
-//経過時間
-let elapsedTime = 0;
-//時間本体
-let t = 0;
-
-//マウスで触れるようにする処理
-ball.onmousedown = function(event) {
-
-state = 2;
-pigmy.setAttribute('src', 'pigmys/pigmy_02.html');
-
-let shiftX = event.clientX - ball.getBoundingClientRect().left;
-let shiftY = event.clientY - ball.getBoundingClientRect().top;
-
-ball.style.position = 'absolute';
-ball.style.zIndex = 1000;
-document.body.append(ball);
-
-moveAt(event.pageX, event.pageY);
-
-// ボールを（pageX、pageY）座標の中心に置く
-function moveAt(pageX, pageY) {
-ball.style.left = pageX - shiftX + 'px';
-ball.style.top = pageY - shiftY + 'px';
-}
-
-function onMouseMove(event) {
-moveAt(event.pageX, event.pageY);
-}
-
-// (3) mousemove でボールを移動する
-document.addEventListener('mousemove', onMouseMove);
-
-// (4) ボールをドロップする。不要なハンドラを削除する
-ball.onmouseup = function() {
-    document.removeEventListener('mousemove', onMouseMove);
-    ball.onmouseup = null;
-    state = 1;
-    arrived = 2;
-    pigmy.setAttribute('src', 'pigmys/pigmy_00.html');
-};
-
-};
-
-ball.ondragstart = function() {
-
-    return false;
-};
-
-//ページが読み込まれたら、リヴリーの位置をウィンドウサイズに合わせて最初にランダム配置する関数
-window.onload = function(e) {
-    const maxY= window.innerHeight -200;
-    const maxX = window.innerWidth -200;
-    const min = 200;
-
-    let shiftX = e.clientX - ball.getBoundingClientRect().left;
-    let shiftY = e.clientY - ball.getBoundingClientRect().top; 
-
-    let randomx = Math.floor( Math.random() * (maxX - min + 1)) + min;
-    let randomy = Math.floor( Math.random() * (maxY - min + 1)) + min;
-
-    console.log()
-    // ボールを（pageX、pageY）座標の中心に置く
-    function moveAt(pageX, pageY) {
-        ball.style.left = pageX + 'px';
-        ball.style.top = pageY + 'px';
-
+//キャラを描画する関数
+function drawing_chara(state){
+    //キャラクターがたってる状態ならば
+    if(state == 0){
+        standing.className = "disp";
+        walking.className = "none";
+        gripped.className = "none";
+        sleeping.className = "none";
+    //キャラクターが歩いてる状態なら
+    }else if(state == 1){
+        standing.className = "none";
+        walking.className = "disp";
+        gripped.className = "none";
+        sleeping.className = "none";
+    //キャラクターがクリックされてるならば
+    }else if(state == 2){
+        standing.className = "none";
+        walking.className = "none";
+        gripped.className = "disp";
+        sleeping.className = "none";
+    //キャラクターが寝てるならば
+    }else{
+        standing.className = "none";
+        walking.className = "none";
+        gripped.className = "none";
+        sleeping.className = "disp";
     }
-    moveAt(randomx, randomy);
-};
-
-//ランダム位置を取得する関数
-function rand_posi(){
-    const maxY= window.innerHeight -200;
-    const maxX = window.innerWidth -200;
-    const min = 200;
-
-    let randomx = Math.floor( Math.random() * (maxX - min + 1)) + min;
-    let randomy = Math.floor( Math.random() * (maxY - min + 1)) + min;
-
-    return [randomx, randomy];
 }
-
-
-
-//リヴリーの位置を取得する関数
-function get_posi(e){
-    let shiftX = ball.getBoundingClientRect().left;
-    let shiftY = ball.getBoundingClientRect().top;
-
-    return [shiftX, shiftY];
-};
 
 //点と点の移動関数any[1][0,1]に移動量の格納
 function calc(fx, fy, tx, ty, move){
@@ -155,78 +76,199 @@ function calc(fx, fy, tx, ty, move){
     }
     return ary;
 }
-//リヴリーの向きを調整する関数
-function directmain(x, x2){
-    const diff_x = x-x2;
 
-    if(diff_x < 0){
-        pigmy.className= "svg-icon";
-    }else{
-        pigmy.className= "svg-icon2";
+//ランダム位置を取得する関数
+function rand_posi(){
+    const maxY= window.innerHeight -200;
+    const maxX = window.innerWidth -200;
+    const min = 200;
+
+    let randomx = Math.floor( Math.random() * (maxX - min + 1)) + min;
+    let randomy = Math.floor( Math.random() * (maxY - min + 1)) + min;
+
+    return [randomx, randomy];
+}
+
+class Chara {
+    constructor(){
+        //目標座標
+        this.aimX = 0;
+        this.aimY = 0;
+
+        //計算用座標
+        this.calcX = 0;
+        this.calcY = 0;
+
+        //前(現在)の座標
+        this.nowX = 0;
+        this.nowY = 0;
+
+        //キャラの状態
+        this.state = 0;
+
+        //キャラのクリック状態
+        this.clicked = 0;
+
+        //キャラが到着したかどうか(0到着,1歩き, 2アイドル)
+        this.arrived = 0;
+
+        //キャラの立ち時間
+        this.idletime = 500;
+
+        //キャラの歩行時間
+        this.walkingtime = 0;
+
+        //立時間処理時間
+        this.time = 0;
+    };
+
+    //クリック状態を変える関数
+    clicke(val, val2){
+        this.clicked = val;
+        this.state = val2;
+        drawing_chara(this.state);
     }
-};
+    //arriveを変える関数
+    arrive(val){
+        this.arrived = val;
+        this.idletime = 120;
+    }
 
-//リヴリーが移動する関数
-function movemain(){
-    //掴まれてなければ
-    if(state == 1){
-        //到着していなければ
-        if(arrived == 0){
-            elapsedTime = 0;
+    //キャラクターの向きを移動方向に対して反転する関数
+    directchara(x, x2){
+        const diff_x = x - x2
 
-            let Now = get_posi();
-            let Random = rand_posi();
-
-            aimX = Random[0];
-            aimY = Random[1];
-
-            nowX = Now[0];
-            nowY = Now[1];
-            angle = (aimY - nowY)/(aimX - nowX);
-
-            ball.style.left = nowX + 'px';
-            ball.style.top = nowY + 'px';
-
-            directmain(nowX, aimX);
-
-            arrived = 1;
-        //進行中
-        }else if(arrived == 1){
-            t -= 2;
-
-            let output = calc(nowX, nowY, aimX , aimY, t);
-
-            ball.style.left = (output[1][0]) + 'px';
-            ball.style.top = (output[1][1]) + 'px';
-
-            //console.log("now:"+ output[1][0], output[1][1]);
-
-            //目的座標についたら到着処理
-            if(aimX == parseInt(output[1][0])){
-                pigmy.setAttribute('src', 'pigmys/pigmy_00.html');
-                arrived = 2;
-
-                //アイドル時間の設定
-                const myArray = [40, 80, 100, 120];
-                let rand = Math.floor(Math.random()*myArray.length);
-
-                idletime = myArray[rand];
-            }
-        //停止中
+        if(diff_x < 0){
+            chara.className= "svg-right";
         }else{
-            if(elapsedTime >= idletime){
-                pigmy.setAttribute('src', 'pigmys/pigmy_01.html');
-                arrived = 0;
-            }else{
-                t=0;
-                elapsedTime += 1;
-                //console.log(elapsedTime);
-            }
+            chara.className= "svg-left";
         }
-    //掴まれていれば
-    }else{
-        
+    };
+
+    //リヴリーの位置を取得する関数
+    get_posi(){
+        this.nowX = chara.getBoundingClientRect().left;
+        this.nowY = chara.getBoundingClientRect().top;
+
+    };
+
+    //キャラクターを移動させる関数
+    movechara(){
+        console.log(this.clicked, this.arrived, this.state);
+        //掴まれていなれば
+        if(this.clicked == 0){
+            //到着していなければ
+            if(this.arrived == 0){   
+                this.walkingtime = 0;         
+                let random = rand_posi();
+                this.aimX = random[0];
+                this.aimY = random[1];
+
+                this.get_posi();
+
+                chara.style.left = this.nowX + 'px';
+                chara.style.top = this.nowY + 'px';
+
+                this.directchara(this.nowX, this.aimX);
+
+                this.state = 1;
+                this.arrived = 1;
+
+                drawing_chara(this.state);
+            //進行中
+            }else if(this.arrived == 1){
+
+                this.walkingtime -= 2;
+
+                let output = calc(this.nowX, this.nowY, this.aimX, this.aimY, this.walkingtime);
+
+                chara.style.left = output[1][0] + 'px';
+                chara.style.top = output[1][1] + 'px';
+
+                if(this.aimX == parseInt(output[1][0])){
+                    this.arrived = 2;
+                    this.walkingtime = 0
+
+                    let whichstate = Math.floor(Math.random() * 100);
+                    if(whichstate < 20){
+                        this.state = 3;
+                        //アイドル時間をランダムで取得
+                        const myArray = [300, 700, 1100];
+                        let rand = Math.floor(Math.random()*myArray.length);
+                        this.idletime = myArray[rand];
+                    }else{
+                        this.state = 0;
+                        //アイドル時間をランダムで取得
+                        const myArray = [40, 80, 100, 120];
+                        let rand = Math.floor(Math.random()*myArray.length);
+                        this.idletime = myArray[rand];
+                    }
+
+                    drawing_chara(this.state);
+                    this.time = 0;
+                }
+            //停止中
+            }else{
+                if(this.time > this.idletime){
+                    drawing_chara(this.state);
+                    this.arrived = 0;
+                }else{
+                    this.time += 1;
+                }
+            }
+        //掴まれてなければ  
+        }else{
+
+        }
+    };
+
+    //繰り返し動く処理
+    loop(){
+        setInterval(this.movechara.bind(this), 30);
     }
 }
 
-setInterval(movemain, 30);
+const livly = new Chara();
+livly.loop();
+
+//マウスで触れるようにする処理
+chara.onmousedown = function(event) {
+
+    livly.clicke(1, 2);
+    
+    let shiftX = event.clientX - chara.getBoundingClientRect().left;
+    let shiftY = event.clientY - chara.getBoundingClientRect().top;
+    
+    chara.style.position = 'absolute';
+    chara.style.zIndex = 1000;
+    //document.body.append(chara);
+    
+    moveAt(event.pageX, event.pageY);
+    
+    //座標の中心に置く
+    function moveAt(pageX, pageY) {
+    chara.style.left = pageX - shiftX + 'px';
+    chara.style.top = pageY - shiftY + 'px';
+    }
+    
+    function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+    }
+    
+    // (3) mousemove で移動する
+    document.addEventListener('mousemove', onMouseMove);
+    
+    // (4) ドロップする。不要なハンドラを削除する
+    chara.onmouseup = function() {
+        document.removeEventListener('mousemove', onMouseMove);
+        chara.onmouseup = null;
+        livly.clicke(0, 0);
+        livly.arrive(2);
+    };
+    
+};
+
+chara.ondragstart = function() {
+
+    return false;
+};
